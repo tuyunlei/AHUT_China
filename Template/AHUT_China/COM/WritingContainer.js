9bccfd6e6544745ee712b7173ecc3dd3dcdfa6a5
小涂 <1206294056@qq.com>
2021-10-01 15:36:55 +0800

    Extract WritingContainer from Description

---
+++ Template/AHUT_China/COM/WritingContainer.js
@@ -0,0 +1,11 @@
+export default {
+    template: `
+   <link rel="stylesheet" type="text/css" href="/IGEM/Template/AHUT_China/CSS/WritingContainer.css">
+
+    <div class="container-fluid py-5 writing-container">
+        <div class="container text-light mt-5 px-5 pt-4 pb-5 rounded-32 lh-lg">
+            <slot></slot>
+        </div>
+    </div>
+    `
+}
