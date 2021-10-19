8aa58da2e62b768fde7d36d339654821b4828c73
小涂 <1206294056@qq.com>
2021-10-19 23:13:12 +0800

    Add NoteBook and Untitled

---
+++ Template/AHUT_China/COM/Notebook.js
@@ -0,0 +1,72 @@
+import WritingContainer from '/Template/AHUT_China/COM/WritingContainer.js'
+
+export default {
+    components: {
+        'writing-container': WritingContainer
+    },
+    template: `
+    <writing-container>
+        <h1>Monthly meetings</h1>
+        <hr/>
+        <p>Why are we doing all of this?</p>
+        <p>Every team's project in iGEM is not perfect at the beginning. In order to keep improving the project, each team holds team meetings to discuss repeatedly, and The team members work better with each other. It was important for our whole team to have meetings on time for some discussions. Our college iGEM project members meet once a week to discuss how to solve the current problems and what to do next. Starting February 10, iGEM members have maintained the habit of meeting once a month for discussion.</p>  
+        <hr/>
+        
+        <h2>February</h2>
+        <p>The meeting includes:</p>
+        <p>Division of labor among the members of this meeting</p>
+        <p>Members of the experimental group introduced our targeted drug experimental project for breast cancer treatment by secreting proteins from probiotics</p>  
+        <p>Through the discussion in this meeting, the division of labor among our members was more clearly defined, and the efficiency among members was greatly improved. At the same time,there was a preliminary understanding of the project we will carry out next.</p>
+        
+        <h2>March</h2>
+        <p>The meeting includes:</p>
+        <p>To investigate the main role of Ecoli-Nissle 1917 bacteria in our project</p>
+        <p>HP and Collaboration's task</p>
+        <p>Task reporting among the HP members</p>
+        <p>Through this meeting, we learned that the experimental group was very sure of the later bacterial utilization and plasmid transformation, and arranged some tasks in the later stage.</p>
+        
+        <h2>April</h2>
+        <p>The meeting includes:</p>
+        <p>The experimental group summarized the basic expertise related to the competition</p>
+        <p>The protocol for HP and Collaboration is preliminarily summarized.The experimental group need to understand the basic ics of genetic engineering</p>
+        <p>Medal standard interpretation</p>
+        
+        <h2>May</h2>
+        <p>The meeting includes:</p>
+        <p>The HP and Collaboration Group should continue their relevant activities.</p>
+        <p>Our group plans to teach students and answer questions.</p>
+        <p>And the teacher was invited to discuss with us.</p>
+        <p>Through the discussion at this conference, we asked the experimental group to conduct an in-depth study of the planned engineered bacterial project at the next stage.At the same time, members of the HP and collabarbitration groups need to investigate and summarize previous Collaboration projects.</p>
+        
+        <h2>June</h2>
+        <p>The meeting includes:</p>
+        <p>The experimental group completed the study, and the instructor to plan the following projects.</p>
+        <p>Members of the HP and Collaboration groups have completed research on the collaboration</p>
+        <p>Through this meeting, we plan to continue to improve the wiki website design for the next stage that needs to comply with this year's theme.At the same time, the experimental group paid attention to collect some relevant experimental pictures used on the dimensional base while performing the experiment.</p>
+        
+        <h2>July</h2>
+        <p>The meeting includes:</p>
+        <p>The Logo production was completed</p>
+        <p>Through this meeting, we plan to meet the next iGEM gold Award requirements for the experimental phase.Prepare different wiki designs for web groups.Through the recent cooperation, the tacit understanding between our team members had improved, and the discussion atmosphere became more and more active.</p>
+        
+        <h2>August</h2>
+        <p>The meeting includes:</p>
+        <p>Strengthen cooperation with the University of Science and Technology of China, have deep understanding of each other's projects, and help each other complete some experiments</p>
+        <p>Improve the logo</p>
+        <p>Through this conference, two different logo designs should be prepared for the web team.</p>
+        <p>Through this meeting, we summarized the design and conception of the project, our team began to design experimental procedures, build plasmid sequences, looking for companies to build plasmid, and after plasmid construction, plasmid began to express and verify pET28a-his-linker-Her2-linker-ISZ-sTRAIL plasmid.</p>
+        
+        <h2>September</h2>
+        <p>The meeting includes:</p>
+        <p>The experimental group was planned to complete our section</p>
+        <p>We voted on the wiki design</p>
+        <p>Through this conference, we summarize and report the experimental results of pET28a-his-linker-ISZ-sTRAIL recombinant vector construction, His-ISZ-sTRAIL protein expression and identification.The results of protein treated cells were discussed and the identification and expression of low oxygen plasmids were planned.</p>
+        
+        <h2>October</h2>
+        <p>The meeting includes:</p>
+        <p>Through this conference, we report the experimental results of low oxygen plasmid expression and identification, and protein-treated cancer cells, checking the accuracy of various test results.</p>
+        <p>Start to check the extent of the project completion and modify it.</p>
+        <p>wiki creation is completed, successively upload manuscripts to complete the Judging Form requirements</p>
+    </writing-container>
+    `
+}
